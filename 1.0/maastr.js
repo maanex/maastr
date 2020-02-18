e332090b2397b508415fe35c8e6a8e59f9490980
maanex <maan91315@gmail.com>
2020-02-18 19:10:54 +0100

    Version 1.0 Release

---
+++ 1.0/maastr.js
@@ -0,0 +1,106 @@
+;/*! maastr 1.0 (c) 2020 Andreas May */
+(function(){
+  'use strict';
+
+  this.maastr = { };
+
+  this.maastr.default = {
+    tokens: {
+      '**': 'bold',
+      '__': 'underline',
+      '~~': 'strikethrough',
+      '*': 'italic'
+    },
+    settings: {
+      htmlTag: 'span',
+      prefix: '',
+      suffix: '',
+    }
+  };
+
+  /**
+   * Parses strings with the ingame format and renders them in HTML
+   * @param {string} string The string to parse
+   */
+  this.maastr.parse = function(string, tokens = maastr.default.tokens, settings = maastr.default.settings) {
+    let ntokens = {};
+    for (let t in tokens) {
+      if (typeof tokens[t] == 'string') ntokens[t]= [ tokens[t], false, false ];
+      else ntokens[t] = [ tokens[t][0], !!tokens[t][1], !!tokens[t][2] ];
+    }
+    tokens = ntokens;
+
+    let active = [ [] ];
+    let lastChar = '';
+    let splitted = string.split('');
+    let out = '';
+    let pos = 0;
+
+    for (let i = 0; i < splitted.length; i++) {
+      let char = splitted[i];
+      let full = lastChar + char;
+      if (full.length == 2 && tokens[full]) {
+        out = out.substr(0, out.length - 1);
+        tokens[full][2] = !tokens[full][2];
+        lastChar = '';
+        pos--;
+      } else if (tokens[char] && !(i+1 < splitted.length && tokens[char + splitted[i+1]])) {
+        tokens[char][2] = !tokens[char][2];
+        lastChar = '';
+      } else {
+        out += char;
+        lastChar = char;
+        pos++;
+      }
+      active[pos] = [];
+      for (let token of Object.values(tokens)) {
+        if (token[2]) active[pos].push(token);
+      }
+    }
+
+    string = out;
+    out = '';
+
+    if (Object.keys(active).length) {
+      for (let i = 0; i < string.length; i++) {
+        let bulk = [];
+        let open = [];
+        let close = 0;
+        for (let token of active[i]) {
+          if (token[1]) bulk.push(token[0]);
+          else {
+            if (i == 0 || !active[i-1].filter(o => o[0] == token[0]).length) {
+              open.push(token[0]);
+            } else if (i == string.length - 1 || !active[i+1].filter(o => o[0] == token[0]).length) {
+              close++;
+            }
+          }
+        }
+
+        let add = string.split('')[i];
+
+        if (bulk.length) {
+          if (add == ' ') add = '&nbsp;';
+          add = `<${settings.htmlTag} class="${bulk.join(' ')}">${add}</${settings.htmlTag}>`;
+        }
+
+        if (open.length) {
+          for (let tag of open)
+            add = `<${settings.htmlTag} class="${tag}">${add}`;
+        }
+
+        if (close) {
+          add += `</${settings.htmlTag}>`.repeat(close);
+        }
+
+        out += add;
+      }
+
+      out = settings.prefix + out + settings.suffix;
+    }
+
+    return out || string;
+  }
+
+  return this.maastr;
+}).call(this);
---
+++ readme.md
@@ -0,0 +1,83 @@
+# maastr
+
+An easy to use "custom text format parser".
+Quick example to explain things:
+```js
+let text = 'My %custom **FORMAT**%';
+let tokens = {
+    '%': 'flipped',
+    '**': 'bold'
+};
+maastr.parse(text, tokens);
+
+// Returns:
+"My <span class="flipped">custom <span class="bold">FORMAT</span></span>"
+```
+
+You can define your own tokens and parse your custom string formatting, ready to be styled in css.
+
+## Getting Started
+
+### Importing the lib
+
+For using it in the browser, either use Github as your CDN and import it as the following:
+```html
+<script src="./maastr.js"></script>
+```
+
+Or choose your version above and download the required files.
+
+An npm module has not yet been created. You can download the files though to use it server-sided.
+
+### Implementing a custom parser
+
+Everything this api has to offer is bundled into the `maastr` object.
+To parse a string, you can use the following method:
+```js
+maastr.parse(string, tokens?, settings?); // returns: string
+```
+
+`tokens` needs to be an object with your tokens as keys and the output classname as values. Example:
+```js
+{
+    '**': 'bold',
+    '__': 'underline',
+    '~~': 'strikethrough',
+    '*': 'italic'
+}
+```
+**The maximum token length for now is limited to two!**
+
+`settings` can be left out for now. For more information, have a look below:
+
+## Advanced
+
+### Settings
+
+The `maastr.parse(...)` method takes in an optional third parameter labeled settings. A settings object looks like this, each parameter is optional and can be left out:
+```js
+{
+    htmlTag: 'span', // The HTML tag of the output. <span> is default but if you need a <div> for example, here's where to change that
+    prefix: '', // Adds text infront of the output. Can be used to wrap the whole generated HTML into another block to target styling more precisely
+    suffix: '', // Same as prefix but after the output
+}
+``` 
+
+### Default Tokens
+
+If you use the same custom formatting everywhere in your project, it can be frustrating to always provide the `parse` method with the tokens. To make things easier, you can set a default `tokens` object using:
+```js
+maastr.default.tokens = { ... }
+```
+
+### Default Settings
+
+Just as you can change the default tokens, you can also change the default settings. Here's how:
+```js
+maastr.default.settings = { ... }
+```
+
+---
+
+You've reached the bottom.
+[Follow me on Twitter](https://twitter.com/Maanex_) and thanks to [Egor](https://twitter.com/egordorichev) for asking me to turn this into a standalone lib.
